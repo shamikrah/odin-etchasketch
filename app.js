3bfbf4bafa4305995e0d86fa0c3bd10666ba70de
Shamik Rahman <shamikrah@gmail.com>
2022-04-05 21:24:13 +1000

    Add hover effect to squares

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.addEventListener('mouseenter', () => square.classList.add('hover'));
         row.appendChild(square);
         if (j == 3) {
             grid.appendChild(row);
