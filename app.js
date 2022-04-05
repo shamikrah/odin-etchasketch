2ac26cc6e1b72d699e074db02d6e15540bd28b05
Shamik Rahman <shamikrah@gmail.com>
2022-04-05 20:53:34 +1000

    Add 16x16 grid

---
+++ app.js
@@ -0,0 +1,18 @@
+const gridContainer = document.querySelector('.grid-container');
+
+const grid = document.createElement('div');
+grid.classList.add('grid');
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div'); 
+    row.classList.add('row');
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        row.appendChild(square);
+        if (j == 3) {
+            grid.appendChild(row);
+        }
+    }
+}
+
+gridContainer.appendChild(grid);
