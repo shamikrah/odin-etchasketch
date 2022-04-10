3053e89518b8f5e5bcc393be8b688e49a8b9ad22
Shamik Rahman <shamikrah@gmail.com>
2022-04-11 00:34:08 +1000

    Finish required functionality

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
-const gridContainer = document.querySelector('.grid-container');
+const container = document.querySelector('.container');
 
 const grid = document.createElement('div');
 grid.classList.add('grid');
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div'); 
-    row.classList.add('row');
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.addEventListener('mouseenter', () => square.classList.add('hover'));
-        row.appendChild(square);
-        if (j == 3) {
-            grid.appendChild(row);
+function createDefaultGrid() {
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement('div'); 
+        row.classList.add('row');
+        for (let j = 0; j < 16; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.addEventListener('mouseenter', () => square.classList.add('hover'));
+            row.appendChild(square);
+            if (j == 15) {
+                grid.appendChild(row);
+            }
         }
     }
 }
+createDefaultGrid();
+container.appendChild(grid);
 
-gridContainer.appendChild(grid);
+const btn = document.querySelector('button');
+let size;
+btn.addEventListener('click', () => {
+    size = prompt('Enter the size of the new grid (maximum 100)', '16');
+    console.log(`New size is ${size}`);
+    createCustomGrid(size);
+});
+
+function resetGrid() {
+    let child = grid.lastElementChild;
+    while (child) {
+        grid.removeChild(child);
+        child = grid.lastElementChild;
+    }
+}
+function createCustomGrid(size) {
+    resetGrid();
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div'); 
+        row.classList.add('row');
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.addEventListener('mouseenter', () => square.classList.add('hover'));
+            row.appendChild(square);
+            if (j == size - 1) {
+                grid.appendChild(row);
+            }
+        }
+    }
+
+}
+
